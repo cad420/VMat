2e2191ed1a8347ddb91178fc6438eb5889c4b3dc
Shuoliu Yang <yangshuoliu@sina.com>
2019-09-25 11:06:08 +0800

    first commit

---
+++ VMat/vmattype.h
@@ -0,0 +1,15 @@
+#ifndef _VMAT_H_
+#define _VMAT_H_
+
+#include <limits>
+
+namespace ysl
+{
+	using Float = float;
+	constexpr Float Pi = 3.14159265358979323846;
+	constexpr Float LOWEST_FLOAT = (std::numeric_limits<Float>::lowest)();
+	constexpr Float MAX_VALUE = (std::numeric_limits<Float>::max)();		// For fucking min/max macro defined in windows.h
+}
+
+
+#endif /*_VMAT_H_*/
